f500d2baf8e5ee06c7b57d90d0dd64df4ed56057
SaraAfreen.Shaik <122810911+saraafreenshaik@users.noreply.github.com>
2023-05-07 22:28:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,54 +1,86 @@
- var audio = document.getElementById("audio");
-       var playBtn = document.getElementById("play");
-       var pauseBtn = document.getElementById("pause");
-       var volumeSlider = document.getElementById("volume");
+ let progress=document.getElementById("progress");
+let song=document.getElementById("song");
+let ctrlIcon=document.getElementById("ctrlIcon");
+
+song.onloadedmetadata=function(){
+  progress.max=song.duration;
+  progress.value=song.currentTime;
+
+}
+function playPause(){
+  if(ctrlIcon.classList.contains("fa-pause")){
+    song.pause();
+    ctrlIcon.classList.remove("fa-pause");
+    ctrlIcon.classList.add("fa-play");
+    
+
+  }
+  else{
+    song.play();
+    ctrlIcon.classList.add("fa-pause");
+    ctrlIcon.classList.remove("fa-play");
+  }
+}
+if(song.play()){
+  setInterval(()=>{
+    progress.value=song.currentTime;
+  },500);
+}
+progress.onchange=function(){
+  song.play();
+  song.currentTime=progress.value;
+  ctrlIcon.classList.add("fa-pause");
+  ctrlIcon.classList.remove("fa-play");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  var audio = document.getElementById("audio");
+//        var playBtn = document.getElementById("play");
+//        var pauseBtn = document.getElementById("pause");
+//        var volumeSlider = document.getElementById("volume");
       
-       playBtn.onclick = function() {
-         audio.play();
-       };
+//        playBtn.onclick = function() {
+//          audio.play();
+//        };
       
-       pauseBtn.onclick = function() {
-         audio.pause();
-       };
+//        pauseBtn.onclick = function() {
+//          audio.pause();
+//        };
       
-       volumeSlider.oninput = function() {
-         audio.volume = volumeSlider.value;
-       };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//        volumeSlider.oninput = function() {
+//          audio.volume = volumeSlider.value;
+//        };
 
 
 
